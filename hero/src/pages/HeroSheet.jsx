cafb76943e31c32fe438ff3201c4ef56f0cef098
YukiTanigaki <yuki.tanigaki.s@gmail.com>
2026-01-11 11:07:18 +0900

    reset

--- hero/src/pages/HeroSheet.jsx
+++
@@ -1,8 +0,0 @@
-export default function HeroSheet() {
-  return (
-    <div style={{ padding: 16 }}>
-      <h1>英雄シート（編集画面）</h1>
-      <p>ここに既存の編集UIを移植していく。</p>
-    </div>
-  );
-}
--- hero/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home({ onStart }) {
-  return (
-    <div style={{ padding: 16 }}>
-      <h1>Home（仮）</h1>
-      <button onClick={onStart}>英雄シートを編集する</button>
-    </div>
-  );
-}
